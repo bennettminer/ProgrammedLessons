3601090820ea1e85111e23d3568f59657cbaa316
Bennett Miner <minerbj@s.dcsdk12.org>
2020-09-08 10:54:33 -0600

    Completed Chapter 9: Exercise 3

--- chapter9/ValuestoVariables.java
+++ chapter9/ValuestoVariables.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ValuestoVariables{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is x equal to? (provide 2 values)");
+        double x = scanner.nextDouble();
+
+        System.out.println("Another:");
+        double x2 = scanner.nextDouble();
+
+        double b = x + x2;
+
+        double c = x * x2;
+        System.out.println("b is: " + b + " c is: " + c);
+        System.out.println("x^2+" + b + "x+" + c + "=0" );
 
     }
 }
