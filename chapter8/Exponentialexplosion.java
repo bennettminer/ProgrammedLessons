81774ecfc2f408b65e283ef3b638c5eb3409f4d0
Bennett Miner <minerbj@s.dcsdk12.org>
2020-09-07 23:50:50 -0600

    Completed Chapter 8: Exercise 3

---
+++ chapter8/Exponentialexplosion.java
@@ -0,0 +1,7 @@
+public class Exponentialexplosion{
+
+    public static void main(String[] args) {
+        double value = 32000000;
+        System.out.println("e to the power value: " + Math.exp( value ) );
+    }
+}
