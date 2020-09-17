af8feab9208fe8d246f813d404ad889abb93d2d0
Bennett Miner <minerbj@s.dcsdk12.org>
2020-09-17 10:42:59 -0600

    Completed Chapter 11: Exercise 1

---
+++ chapter11/runTime.java
@@ -0,0 +1,9 @@
+public class runTime {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String str2 = "";
+
+        System.out.println( str );
+        System.out.println( str2 );
+    }
+}
